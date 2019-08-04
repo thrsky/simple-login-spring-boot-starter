aa0cd03abc752c7b9aa4726061044dafd86f5f9c
厉康 <thrsky@163.com>
2019-08-04 22:35:26 +0800

    add datasource package

---
+++ src/main/java/org/thrsky/spring/boot/login/datasource/H2DataSource.java
@@ -0,0 +1,21 @@
+package org.thrsky.spring.boot.login.datasource;
+
+/**
+ * @author thrsky
+ * @version 1.0.0
+ * @date Created in 18:54 2019-08-04
+ */
+public class H2DataSource {
+
+    /**
+     * h2 配置
+     * spring.datasource.url=jdbc:h2:mem:testdb
+     * spring.datasource.driverClassName=org.h2.Driver
+     * spring.datasource.username=sa
+     * spring.datasource.password=password
+     * spring.jpa.database-platform=org.hibernate.dialect.H2Dialect
+     *
+     * h2本地持久化文件
+     * spring.datasource.url=jdbc:h2:file:/data/demo
+     */
+}
