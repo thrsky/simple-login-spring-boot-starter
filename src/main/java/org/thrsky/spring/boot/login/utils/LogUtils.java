6853e29c551204f1e9f294bab6f98a7b709fb40a
厉康 <thrsky@163.com>
2019-08-04 22:40:19 +0800

    add logUtils

---
+++ src/main/java/org/thrsky/spring/boot/login/utils/LogUtils.java
@@ -0,0 +1,37 @@
+package org.thrsky.spring.boot.login.utils;
+
+import com.google.common.base.Throwables;
+import org.slf4j.Logger;
+
+/**
+ * @author thrsky
+ * @version 1.0.0
+ * @date Created in 17:32 2019-08-04
+ */
+public class LogUtils {
+
+    private static final String LOG_SEPARATOR = ",";
+
+    private LogUtils() {
+        //禁用构造函数
+    }
+
+    public static void info(Logger logger, String... msg) {
+        logger.info(buildLogString(null, msg));
+    }
+
+    public static void info(Logger logger, Throwable throwable, String... msg) {
+        logger.info(buildLogString(throwable, msg));
+    }
+
+    private static String buildLogString(Throwable throwable, String... msg) {
+        StringBuilder sb = new StringBuilder();
+        if (throwable != null) {
+            sb.append(Throwables.getStackTraceAsString(throwable)).append(LOG_SEPARATOR);
+        }
+        for (String s : msg) {
+            sb.append(s).append(LOG_SEPARATOR);
+        }
+        return sb.toString();
+    }
+}
