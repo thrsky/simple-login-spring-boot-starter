ebcc7eb2d5b7be6f0240d6427f1a4d50f817c6dd
厉康 <thrsky@163.com>
2019-09-07 23:42:46 +0800

    增加用户数据配置类

---
+++ src/main/java/org/thrsky/spring/boot/login/properties/UserDataConfig.java
@@ -0,0 +1,26 @@
+package org.thrsky.spring.boot.login.properties;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+/**
+ * @author thrsky
+ * @version 1.0.0
+ * @date Created in 11:19 2019-09-07
+ */
+@ConfigurationProperties("simple.login.user.data")
+@Data
+public class UserDataConfig {
+
+    private String location;
+
+    public static class MysqlConfig {
+        private String url;
+        private String username;
+        private String password;
+    }
+
+    public static class LocalFileConfig {
+        private String filepath;
+    }
+}
