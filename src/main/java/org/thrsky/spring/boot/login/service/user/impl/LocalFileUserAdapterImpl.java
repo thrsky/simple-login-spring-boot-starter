e68787dbe10ff2fd3dbe576bb80c661f8168456d
厉康 <thrsky@163.com>
2019-09-07 23:43:16 +0800

    初始化 用户service具体实现

--- src/main/java/org/thrsky/spring/boot/login/service/user/impl/LocalFileUserAdapterImpl.java
+++ src/main/java/org/thrsky/spring/boot/login/service/user/impl/LocalFileUserAdapterImpl.java
@@ -2,6 +2,7 @@ package org.thrsky.spring.boot.login.service.user.impl;
 
 import org.thrsky.spring.boot.login.exception.BaseBizException;
 import org.thrsky.spring.boot.login.model.UserModel;
+import org.thrsky.spring.boot.login.properties.UserDataConfig;
 import org.thrsky.spring.boot.login.service.user.UserAdapter;
 
 /**
@@ -10,6 +11,17 @@ import org.thrsky.spring.boot.login.service.user.UserAdapter;
  * @date Created in 22:38 2019-08-04
  */
 public class LocalFileUserAdapterImpl implements UserAdapter {
+
+    private UserDataConfig.LocalFileConfig config;
+
+    public LocalFileUserAdapterImpl(UserDataConfig.LocalFileConfig config) {
+        this.config = config;
+        this.init();
+    }
+
+    private void init() {
+
+    }
 
     @Override
     public boolean login(UserModel userModel) throws BaseBizException {
--- src/main/java/org/thrsky/spring/boot/login/service/user/impl/MysqlUserAdapterImpl.java
+++ src/main/java/org/thrsky/spring/boot/login/service/user/impl/MysqlUserAdapterImpl.java
@@ -2,6 +2,7 @@ package org.thrsky.spring.boot.login.service.user.impl;
 
 import org.thrsky.spring.boot.login.exception.BaseBizException;
 import org.thrsky.spring.boot.login.model.UserModel;
+import org.thrsky.spring.boot.login.properties.UserDataConfig;
 import org.thrsky.spring.boot.login.service.user.UserAdapter;
 
 /**
@@ -10,6 +11,17 @@ import org.thrsky.spring.boot.login.service.user.UserAdapter;
  * @date Created in 22:37 2019-08-04
  */
 public class MysqlUserAdapterImpl implements UserAdapter {
+
+    private UserDataConfig.MysqlConfig mysqlConfig;
+
+    public MysqlUserAdapterImpl(UserDataConfig.MysqlConfig config) {
+        this.mysqlConfig = config;
+        init();
+    }
+
+    private void init() {
+
+    }
 
     @Override
     public boolean login(UserModel userModel) throws BaseBizException {
