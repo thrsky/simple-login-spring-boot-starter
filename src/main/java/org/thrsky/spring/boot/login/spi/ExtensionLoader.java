c78bdcb5eb9ab0c0c0547c273778fbf4ff7e5cb3
厉康 <thrsky@163.com>
2019-09-10 23:14:35 +0800

    增加 SPI 模块

---
+++ src/main/java/org/thrsky/spring/boot/login/spi/ExtensionLoader.java
@@ -0,0 +1,16 @@
+package org.thrsky.spring.boot.login.spi;
+
+/**
+ * @author thrsky
+ * @version 1.0.0
+ * @date Created in 23:13 2019-09-10
+ */
+public class ExtensionLoader {
+
+    /**
+     * 加载用户自定义的Service
+     */
+    public void load() {
+        //todo
+    }
+}
